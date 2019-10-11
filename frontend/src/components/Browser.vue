aabc5176b119ecfb0bcdd126788e5d4fefab4b7f
Rajdeep Bharati <rajdeep@macports.org>
2019-10-11 22:15:37 +0530

    Initialize Vue CLI app

---
+++ frontend/src/components/Browser.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div v-for="(item, index) in listItems" :key="index">
+      {{ item }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Browser',
+  components: {},
+  data() {
+    return {
+      listItems: []
+    }
+  }
+}
+</script>
+<style scoped></style>
